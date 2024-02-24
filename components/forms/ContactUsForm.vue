76b55c0e5cb27e1b35611225231fff13c9e99595
alex pabouctsidis <alex.pabouct@gmail.com>
2024-02-24 15:45:04 +0100

    typo fix

--- components/forms/ContactUsForm.vue
+++ components/forms/ContactUsForm.vue
@@ -94,7 +94,7 @@
               </div>
             </div>
             <div class="sm:col-span-2">
-              <label for="country" class="block text-sm font-semibold leading-6 text-gray-900">Company</label>
+              <label for="country" class="block text-sm font-semibold leading-6 text-gray-900">Country</label>
               <div class="mt-2.5">
                 <input v-model="formData.country" type="country" name="country" id="country" autocomplete="country"
                   class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
